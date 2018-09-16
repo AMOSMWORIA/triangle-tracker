589261cb89185bf3626a4bed0579b16649757865
AMOSMWORIA <amosmworia007@gmail.com>
2018-09-16 17:49:55 +0300

    intro the code that calculates if numbers given form a triangle

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var a=parseInt(prompt("input 1"))
+var b=parseInt(prompt("input 2"))
+var c=parseInt(prompt("input 3"))
+
+
+
+var Triangle = true
+
+if ( a+b<=c||a+c<=d||b+c<=a ){
+   console.log("notTriangle") 
+}
+else if (a===b&&a===c&&b===c) {
+  console.log("equilateral")
+} 
+else if (a===b||a===c||b===c &&a!==b||b!==c||c!==a){
+   console.log("Isosceles") 
+}
+else if (a!==b||a!==c&&b!==c){
+   console.log("scalene") 
+}
